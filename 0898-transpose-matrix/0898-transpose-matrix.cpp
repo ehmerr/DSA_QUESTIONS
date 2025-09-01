1a15220eb834fc2b42765f3d9e013b437f835893
Ahmar Rahmani <159912300+ehmerr@users.noreply.github.com>
2025-09-01 11:46:35 +0530

    Time: 0 ms (100%), Space: 15.1 MB (35.54%) - LeetHub

--- 0898-transpose-matrix/0898-transpose-matrix.cpp
+++ 0898-transpose-matrix/0898-transpose-matrix.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-        //we are creating a matrix which will have rows equal to the columns of original matrix and columns equal to the rows of original matrix
-
-        //rows in the original matrix
-        int rowsOriginal = matrix.size();
-        int colsOriginal = matrix[0].size();
-
-        int rowsTranspose = colsOriginal;
-        int colsTranspose = rowsOriginal;
-
-        //initialising the transpose matrix
-        vector<vector<int>> transpose(rowsTranspose,vector<int>(colsTranspose,0));
-  
-  // we traverse through the transpose matrix
-        for(int i=0;i<rowsTranspose;i++){
-            for(int j=0;j<colsTranspose;j++){
-                //we swap the elements of positon [i,j] of transpose matrix with elements of position [j,i] of the original matrix
-                transpose[i][j]=matrix[j][i];
-            }
-        }
-        return transpose;
         
+int rowsOrig = matrix.size();
+int colsOrig = matrix[0].size();
+
+int rowsTrans = colsOrig;
+int colsTrans = rowsOrig;
+
+   vector<vector<int>> transpose(rowsTrans, vector<int>(colsTrans,0));
+
+for(int i=0;i<rowsTrans;i++){
+    for(int j=0;j<colsTrans;j++){
+        transpose[i][j] = matrix[j][i];
+        }
     }
+    return transpose;
+}
+
 };
